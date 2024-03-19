c59d48c8b36ba56c87a9066dfa9b69fee05d6691
Siarhei Sauras <sergei.sovras@gmail.com>
2024-03-19 20:00:54 +0300

    Task #7_1 was completed using the while loop

---
+++ homework/sergei_sovras/Homework_7/Task_7_1.py
@@ -0,0 +1,11 @@
+quiz = 5
+while True:
+    answer = input('Enter the number or type end to leave: ')
+    if answer == 'end':
+        break
+    elif quiz == int(answer):
+        print('You are lucky!')
+        break
+    else:
+        print('You are wrong( Try again')
+        
