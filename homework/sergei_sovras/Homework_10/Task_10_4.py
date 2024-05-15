d0bc711361e256babb18d02b4c8d6fe2f1d9c2b7
Siarhei Sauras <sergei.sovras@gmail.com>
2024-05-15 22:13:20 +0300

    The 10_4 was fixed

--- homework/sergei_sovras/Homework_10/Task_10_4.py
+++ homework/sergei_sovras/Homework_10/Task_10_4.py
@@ -6,12 +6,11 @@ PRICE_LIST = '''тетрадь 50р
 пенал 300р
 рюкзак 500р'''
 
-new_list = PRICE_LIST.split('\n')
-item = []
-cost = []
-for i in range(len(new_list)):
-    x, y = new_list[i].split(' ')
-    item.append(x)
-    cost.append(int(y[:-1]))
-    i += 1
-print(dict(zip(item, cost)))
+
+def creating_price(new_list):
+    new_list = dict([x.split(' ') for x in new_list.split('\n')])
+    new_list = {x: int(y[:-1]) for x, y in new_list.items()}
+    return new_list
+
+
+print(creating_price(PRICE_LIST))
