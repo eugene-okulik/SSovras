c5657a842310c2d7d5df78ee1256de05faea70e2
serge <sergei.sovras@gmail.com>
2024-07-27 19:28:24 +0300

    SQL Queries v2 (with Python)

--- homework/sergei_sovras/Homework_15/Task_15.py
+++ homework/sergei_sovras/Homework_15/Task_15.py
@@ -109,17 +109,17 @@ print(cursor.fetchall())
 
 # 9
 query_9 = f'''
-SELECT s.id Student_id, s.name First_name, s.second_name Second_name, g.title Grpoup_title, g.start_date Start_date, 
+SELECT s.id Student_id, s.name First_name, s.second_name Second_name, g.title Grpoup_title, g.start_date Start_date,
 g.end_date End_date, b.title Book_title, mls.Mark Mark_value, mls.Lesson_id Lesson_id, mls.Subject_title Subject_title
-FROM students s 
-JOIN `groups` g ON s.group_id = g.id 
+FROM students s
+JOIN `groups` g ON s.group_id = g.id
 JOIN books b ON s.id = b.taken_by_student_id
 JOIN (
     SELECT j1.Stud_id, j1.markid Mark_id, j1.Mark, j1.t1 Lesson_id, j2.stil Subject_title FROM (
-        SELECT m.id markid, m.value Mark, m.student_id Stud_id, l.id lid, l.title t1 FROM marks m 
+        SELECT m.id markid, m.value Mark, m.student_id Stud_id, l.id lid, l.title t1 FROM marks m
             JOIN lessons l ON m.lesson_id = l.id) j1
             JOIN (
-                SELECT l.id lid2, l.title t2, s.title stil FROM lessons l 
+                SELECT l.id lid2, l.title t2, s.title stil FROM lessons l
                     JOIN subjets s ON l.subject_id = s.id
                 ) j2 ON j1.lid = j2.lid2
         ) mls ON s.id = mls.Stud_id
