63c12359ef27b86b13e93e8726f2ae3d309d0c7d
serge <sergei.sovras@gmail.com>
2024-07-27 19:24:17 +0300

    SQL Queries v2 (with Python)

--- homework/sergei_sovras/Homework_15/Task_15.py
+++ homework/sergei_sovras/Homework_15/Task_15.py
@@ -1,6 +1,12 @@
 import mysql.connector as mysql
 
-db = mysql.connect()
+db = mysql.connect(
+    host='db-mysql-fra1-09136-do-user-7651996-0.b.db.ondigitalocean.com',
+    port=25060,
+    user='st-onl',
+    passwd='AVNS_tegPDkI5BlB2lW5eASC',
+    database='st-onl'
+)
 cursor = db.cursor(dictionary=True)
 
 # 1 INSERT INTO students (name, second_name) VALUES ('Arnold', 'Arni_Tester')
