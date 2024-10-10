32f66025eb73de41eacb7812ec8d61204a0c7358
serge <sergei.sovras@gmail.com>
2024-10-10 21:23:59 +0300

    The 1st task is completed

---
+++ homework/sergei_sovras/homework_23/task_23_1.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+@pytest.fixture()
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    yield chrome_driver
+
+
+def test_visit_and_fill_text(driver):
+    url = 'https://www.qa-practice.com/elements/input/simple'
+    input_text = 'Test_input'
+    driver.get(url)
+    input_field = driver.find_element(By.ID, 'id_text_string')
+    input_field.send_keys(input_text)
+    input_field.submit()
+    result_field = driver.find_element(By.ID, 'result-text')
+    assert result_field.text == input_text, "Input text doesn't match the result"
+    print(result_field.text)
