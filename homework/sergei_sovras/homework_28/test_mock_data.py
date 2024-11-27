11e4594256c6515c99523f24887c2bbfaee30a42
serge <sergei.sovras@gmail.com>
2024-11-27 15:50:12 +0300

    Test for data mocking

--- homework/sergei_sovras/homework_28/test_mock_data.py
+++ homework/sergei_sovras/homework_28/test_mock_data.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, Route
 import json
 
+
 def test_mock_data(page: Page):
 
     def handle_and_update_rout(route: Route):
@@ -9,16 +10,12 @@ def test_mock_data(page: Page):
         body = response.json()
         body['body']['digitalMat'][0]['familyTypes'][0]['productName'] = title_update
         body = json.dumps(body)
-        route.fulfill(
-           response=response,
-            body=body
-        )
+        route.fulfill(response=response,body=body)
 
     page.route('**/digital-mat?path=library/step0_iphone/digitalmat', handle_and_update_rout)
     page.goto('https://www.apple.com/shop/buy-iphone')
     iphone_item = page.get_by_role('heading', name='iPhone 16 Pro &')
     iphone_item.click()
-
     popup = page.locator(
         '[class="rf-digitalmat-overlay-contentsection"]'
     ).locator('#rf-digitalmat-overlay-label-0').nth(0)
